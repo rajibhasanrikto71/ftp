c2617945f712c15895cfe1f87c87e6bcc4ae7be1
Villain <83833885+rajibhasanrikto71@users.noreply.github.com>
2022-03-15 00:05:06 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+f = document.querySelector(".but6");
+g = document.querySelector(".but7");
+villain = document.querySelector(".but71");
+
+x.innerHTML="Villain";
+a.innerHTML="Bangla Plex";
+b.innerHTML="PCV";
+c.innerHTML="Chocolate";
+d.innerHTML="Media PLEX";
+e.innerHTML="FNF Online";
+f.innerHTML="Drive Movie";
+g.innerHTML="Development";
+villain.innerHTML="Black Web 71";
